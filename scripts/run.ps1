9867eb70fee8a45c04e1aab78b16e7a5dc2261a3
yuan-shuo <369599207@qq.com>
2026-05-14 18:08:20 +0800

    chore: 发布v0.2.1版本并更新脚本
    1.  将版本号从0.2.0更新至0.2.1
    2.  在run.ps1中添加版本查看相关的运行命令

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,3 +1,7 @@
 go run main.go lf zerotele.example.yaml -d .\testoutput\logger
 go run main.go lf zerotele.example.yaml -d .\testoutput\logger -m mask.go
 go run main.go met zerotele.example.yaml -d .\testoutput\metrics
+
+go run main.go -v
+go run main.go --version
+go run main.go version
